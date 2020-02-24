9ceb8d8a00e1c4704c2fb8dd79d687a3ac4d71d0
kirilpopovv2002 <kiril.popov.v@yandex.by>
2020-02-24 09:33:38 +0300

    2

---
+++ ConsoleApp1/ConsoleApp1/Gun.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Gun
+    {
+        private string Name { get; set; }
+        private float Caliber { get; set; }
+        private double Range { get; set; }
+
+    }
+}
