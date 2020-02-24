39d1fe75dec86ff886f7e66c3531878aefe33e57
kirilpopovv2002 <kiril.popov.v@yandex.by>
2020-02-24 10:29:52 +0300

    4

--- ConsoleApp1/ConsoleApp1/Gun.cs
+++ ConsoleApp1/ConsoleApp1/Gun.cs
@@ -6,18 +6,55 @@ namespace ConsoleApp1
 {
     class Gun
     {
-        private string Name { get; set; }
-        private float Caliber { get; set; }
-        private double Range { get; set; }
+        private string _Name { get; set; }
+        private double _Caliber { get; set; }
+        private double _Range { get; set; }
         public Gun()
         {
+            this._Name = "Now name";
+            this._Caliber = 0;
+            this._Range = 0;
         }
-        public Gun(string _Name,float _Caliber, double _Range)
+        public Gun(string _name, double _caliber, double _range)
         {
-            this.Name = _Name;
-            this.Caliber = _Caliber;
-            this.Range = _Range;
+            this._Name = _name;
+            this._Caliber = _caliber;
+            this._Range = _range;
+        }
+        public string NameEdit(string _name)
+        {
+            this._Name = _name;
+            return _name;
+        }
+        public double CaliberEdit(double _caliber)
+        {
+            this._Caliber = _caliber;
+            return _caliber;
+        }
+        public double RangeEdit(double _range)
+        {
+            this._Range = _range;
+            return _range;
         }
 
+        public string NameInfo()
+        {
+            
+            return this._Name;
+        }
+        public double CaliberInfo()
+        {
+            
+            return this._Caliber;
+        }
+        public double RangeInfo()
+        {
+            
+            return this._Range;
+        }
+        public override string ToString()
+        {
+            return string.Format("Название:{0}\nКалибр:{1}\nДальность:{2}",this._Name,this._Caliber,this._Range);
+        }
     }
 }
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,16 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Gun gun = new Gun();
+            Console.WriteLine("введите название");
+            gun.NameEdit(Console.ReadLine());
+            Console.WriteLine("введите калибр");
+            gun.CaliberEdit(Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine("введите дальность");
+            gun.RangeEdit(Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine("Название:{0}\nКалибр:{1}\nДальность:{2}",gun.NameInfo(), gun.CaliberInfo(), gun.RangeInfo());
+            gun.ToString();
+            Console.ReadKey();
         }
     }
 }
